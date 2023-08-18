8910e4d6961548e7170d4edc221ecd4853fcbbf6
alamsyahh15 <black.taurus001@gmail.com>
2023-08-18 13:53:00 +0700

    adding path asset to pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,11 @@ import 'package:rumah_kreatif_toba/controllers/cart_controller.dart';
 import 'package:rumah_kreatif_toba/controllers/pesanan_controller.dart';
 import 'package:rumah_kreatif_toba/controllers/popular_produk_controller.dart';
 import 'package:rumah_kreatif_toba/controllers/wishlist_controller.dart';
-import 'package:rumah_kreatif_toba/pages/home/home_page.dart';
-import 'package:rumah_kreatif_toba/pages/home/home_page_body.dart';
-import 'package:rumah_kreatif_toba/pages/home/main_home_page.dart';
-import 'package:rumah_kreatif_toba/pages/keranjang/keranjang_page.dart';
-import 'package:rumah_kreatif_toba/pages/pembelian/pembelian_page.dart';
-import 'package:rumah_kreatif_toba/pages/produk/produk_detail.dart';
-import 'package:rumah_kreatif_toba/pages/wishlist/wishlist_page.dart';
 import 'package:rumah_kreatif_toba/routes/route_helper.dart';
-import 'controllers/auth_controller.dart';
+
 import 'controllers/bank_controller.dart';
 import 'controllers/user_controller.dart';
 import 'helper/dependencies.dart' as dep;
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:change_app_package_name/change_app_package_name.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
