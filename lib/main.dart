e3d8ab6b76f76f66fc65709c1b19cf1ae903f5ff
alamsyahh15 <black.taurus001@gmail.com>
2023-08-18 19:44:53 +0700

    sync build number

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ import 'controllers/bank_controller.dart';
 import 'controllers/user_controller.dart';
 import 'helper/dependencies.dart' as dep;
 
+// Untuk naikan version cukup via file pubspec.yaml saja
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dep.init();
